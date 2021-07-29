4bf9e0edbba8b0ee7aee68b2d01d0998805e1520
via5 <65577279+via5@users.noreply.github.com>
2021-07-29 13:37:14 -0400

    split

---
+++ project/src/MorphUI/Controls.cs
@@ -0,0 +1,40 @@
+﻿namespace AUI.MorphUI
+{
+	class Controls : VUI.Panel
+	{
+		private readonly MorphUI ui_;
+		private VUI.IntTextSlider page_ = new VUI.IntTextSlider();
+		private VUI.IgnoreFlag ignore_ = new VUI.IgnoreFlag();
+
+		public Controls(MorphUI ui)
+		{
+			ui_ = ui;
+
+			var p = new VUI.Panel(new VUI.HorizontalFlow());
+
+			p.Add(new VUI.Label("Page: "));
+			p.Add(page_);
+			p.Add(new VUI.ToolButton("<", () => ui_.PreviousPage()));
+			p.Add(new VUI.ToolButton(">", () => ui_.NextPage()));
+
+			Layout = new VUI.BorderLayout();
+			Add(p, VUI.BorderLayout.Center);
+
+			page_.ValueChanged += OnPageChanged;
+		}
+
+		public void Update()
+		{
+			ignore_.Do(() =>
+			{
+				page_.Set(ui_.CurrentPage + 1, 1, ui_.PageCount);
+			});
+		}
+
+		private void OnPageChanged(int p)
+		{
+			if (ignore_) return;
+			ui_.CurrentPage = page_.Value - 1;
+		}
+	}
+}
---
+++ project/src/MorphUI/MorphPanel.cs
@@ -0,0 +1,124 @@
+﻿namespace AUI.MorphUI
+{
+	class MorphPanel : VUI.Panel
+	{
+		private DAZMorph morph_ = null;
+
+		private VUI.Label name_ = new VUI.Label();
+		private VUI.FloatTextSlider slider_ = new VUI.FloatTextSlider("0.000");
+		private VUI.ToolButton reset_ = new VUI.ToolButton("R");
+		private VUI.ToolButton def_ = new VUI.ToolButton("Def");
+		private VUI.ToolButton addRange_ = new VUI.ToolButton("+Range");
+		private VUI.ToolButton resetRange_ = new VUI.ToolButton("R range");
+		private VUI.CheckBox fav_ = new VUI.CheckBox("F");
+
+		private VUI.IgnoreFlag ignore_ = new VUI.IgnoreFlag();
+
+		public MorphPanel()
+		{
+			int buttonsFontSize = 18;
+
+			name_.WrapMode = VUI.Label.ClipEllipsis;
+			name_.FontSize = 24;
+
+			Padding = new VUI.Insets(5);
+			Borders = new VUI.Insets(1);
+			Layout = new VUI.VerticalFlow();
+
+			var buttons = new VUI.Panel(new VUI.HorizontalFlow(
+				3, VUI.FlowLayout.AlignLeft | VUI.FlowLayout.AlignVCenter));
+			buttons.Add(reset_);
+			buttons.Add(def_);
+			buttons.Add(addRange_);
+			buttons.Add(resetRange_);
+			buttons.Add(fav_);
+
+			reset_.FontSize = buttonsFontSize;
+			def_.FontSize = buttonsFontSize;
+			addRange_.FontSize = buttonsFontSize;
+			resetRange_.FontSize = buttonsFontSize;
+			fav_.FontSize = buttonsFontSize;
+
+			Add(name_);
+			Add(slider_);
+			Add(buttons);
+
+			slider_.ValueChanged += OnValue;
+			reset_.Clicked += OnReset;
+			def_.Clicked += OnDefault;
+			addRange_.Clicked += OnAddRange;
+			resetRange_.Clicked += OnResetRange;
+			fav_.Changed += OnFavorite;
+		}
+
+		public void SetMorph(DAZMorph m)
+		{
+			morph_ = m;
+
+			if (morph_ == null)
+			{
+				Render = false;
+			}
+			else
+			{
+				Render = true;
+				name_.Text = m.displayName;
+				name_.Tooltip.Text =
+					$"{m.displayName}\n\n" +
+					$"{m.uid}\n\n" +
+					$"{m.metaLoadPath}";
+			}
+
+			Update();
+		}
+
+		public void Update()
+		{
+			if (morph_ == null) return;
+
+			ignore_.Do(() =>
+			{
+				slider_.Set(morph_.morphValue, morph_.min, morph_.max);
+				fav_.Checked = morph_.favorite;
+			});
+		}
+
+		private void OnValue(float f)
+		{
+			if (ignore_ || morph_ == null) return;
+			morph_.morphValue = f;
+		}
+
+		private void OnReset()
+		{
+			if (ignore_ || morph_ == null) return;
+			morph_.ResetRange();
+			slider_.Set(morph_.startValue, morph_.min, morph_.max);
+		}
+
+		private void OnDefault()
+		{
+			if (ignore_ || morph_ == null) return;
+			slider_.Value = morph_.startValue;
+		}
+
+		private void OnAddRange()
+		{
+			if (ignore_ || morph_ == null) return;
+			slider_.SetRange(slider_.Minimum - 1, slider_.Maximum + 1);
+		}
+
+		private void OnResetRange()
+		{
+			if (ignore_ || morph_ == null) return;
+			morph_.ResetRange();
+			slider_.SetRange(morph_.min, morph_.max);
+		}
+
+		private void OnFavorite(bool b)
+		{
+			if (ignore_ || morph_ == null) return;
+			morph_.favorite = b;
+		}
+	}
+}
