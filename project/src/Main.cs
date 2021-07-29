c12edc9a485e857ed4e3ff88c45c8c70e5cc7eb8
via5 <65577279+via5@users.noreply.github.com>
2021-07-29 11:34:41 -0400

    split

--- project/src/Main.cs
+++ project/src/Main.cs
@@ -1,132 +1,11 @@
-﻿using System.Collections.Generic;
-
-namespace AUI
+﻿namespace AUI
 {
-	class MorphPanel : VUI.Panel
-	{
-		private DAZMorph morph_ = null;
-
-		private VUI.Label name_ = new VUI.Label();
-		private VUI.FloatTextSlider slider_ = new VUI.FloatTextSlider("0.000");
-		private VUI.ToolButton reset_ = new VUI.ToolButton("R");
-		private VUI.ToolButton def_ = new VUI.ToolButton("Def");
-		private VUI.ToolButton addRange_ = new VUI.ToolButton("+Range");
-		private VUI.ToolButton resetRange_ = new VUI.ToolButton("R range");
-		private VUI.CheckBox fav_ = new VUI.CheckBox("F");
-
-		private VUI.IgnoreFlag ignore_ = new VUI.IgnoreFlag();
-
-		public MorphPanel()
-		{
-			int buttonsFontSize = 18;
-
-			name_.WrapMode = VUI.Label.ClipEllipsis;
-			name_.FontSize = 24;
-
-			Padding = new VUI.Insets(5);
-			Borders = new VUI.Insets(1);
-			Layout = new VUI.VerticalFlow();
-
-			var buttons = new VUI.Panel(new VUI.HorizontalFlow(
-				3, VUI.FlowLayout.AlignLeft | VUI.FlowLayout.AlignVCenter));
-			buttons.Add(reset_);
-			buttons.Add(def_);
-			buttons.Add(addRange_);
-			buttons.Add(resetRange_);
-			buttons.Add(fav_);
-
-			reset_.FontSize = buttonsFontSize;
-			def_.FontSize = buttonsFontSize;
-			addRange_.FontSize = buttonsFontSize;
-			resetRange_.FontSize = buttonsFontSize;
-			fav_.FontSize = buttonsFontSize;
-
-			Add(name_);
-			Add(slider_);
-			Add(buttons);
-
-			slider_.ValueChanged += OnValue;
-			reset_.Clicked += OnReset;
-			def_.Clicked += OnDefault;
-			addRange_.Clicked += OnAddRange;
-			resetRange_.Clicked += OnResetRange;
-			fav_.Changed += OnFavorite;
-		}
-
-		public void SetMorph(DAZMorph m)
-		{
-			morph_ = m;
-
-			name_.Text = m.displayName;
-			name_.Tooltip.Text =
-				$"{m.displayName}\n\n" +
-				$"{m.uid}\n\n" +
-				$"{m.metaLoadPath}";
-
-			Update();
-		}
-
-		public void Update()
-		{
-			if (morph_ == null) return;
-
-			ignore_.Do(() =>
-			{
-				slider_.Set(morph_.morphValue, morph_.min, morph_.max);
-				fav_.Checked = morph_.favorite;
-			});
-		}
-
-		private void OnValue(float f)
-		{
-			if (ignore_) return;
-			morph_.morphValue = f;
-		}
-
-		private void OnReset()
-		{
-			if (ignore_) return;
-			morph_.ResetRange();
-			slider_.Set(morph_.startValue, morph_.min, morph_.max);
-		}
-
-		private void OnDefault()
-		{
-			if (ignore_) return;
-			slider_.Value = morph_.startValue;
-		}
-
-		private void OnAddRange()
-		{
-			if (ignore_) return;
-			slider_.SetRange(slider_.Minimum - 1, slider_.Maximum + 1);
-		}
-
-		private void OnResetRange()
-		{
-			if (ignore_) return;
-			morph_.ResetRange();
-			slider_.SetRange(morph_.min, morph_.max);
-		}
-
-		private void OnFavorite(bool b)
-		{
-			if (ignore_) return;
-			morph_.favorite = b;
-		}
-	}
-
-
 	class AlternateUI : MVRScript
 	{
 		static private AlternateUI instance_ = null;
 
+		private MorphUI mui_ = new MorphUI();
 		private bool inited_ = false;
-		private VUI.Root root_ = null;
-		private VUI.Panel panel_ = new VUI.Panel();
-		private List<MorphPanel> panels_ = new List<MorphPanel>();
-		private List<DAZMorph> morphs_ = new List<DAZMorph>();
-		private List<DAZMorph> filtered_ = new List<DAZMorph>();
 
 		public AlternateUI()
 		{
@@ -138,146 +17,23 @@ namespace AUI
 			get { return instance_; }
 		}
 
-		private void DoInit()
-		{
-			VUI.Glue.Set(
-				() => manager,
-				(s, ps) => string.Format(s, ps),
-				(s) => SuperController.LogError(s),
-				(s) => SuperController.LogError(s),
-				(s) => SuperController.LogError(s),
-				(s) => SuperController.LogError(s));
-
-			root_ = new VUI.Root(UITransform.GetComponentInChildren<MVRScriptUI>());
-			root_.ContentPanel.Layout = new VUI.BorderLayout();
-			root_.ContentPanel.Add(panel_, VUI.BorderLayout.Center);
-
-			int cols = 3;
-			int rows = 5;
-
-			var gl = new VUI.GridLayout(cols);
-			gl.UniformWidth = true;
-			gl.Spacing = 5;
-			panel_.Layout = gl;
-
-			for (int i = 0; i < cols * rows; ++i)
-			{
-				var p = new MorphPanel();
-				panels_.Add(p);
-				panel_.Add(p);
-			}
-		}
-
-		public void FixedUpdate()
-		{
-		}
-
 		public void Update()
 		{
-			if (!inited_ && UITransform != null)
+			U.Safe(() =>
 			{
-				DoInit();
-				inited_ = true;
-				DoUpdate();
-			}
-
-			if (root_ != null)
-			{
-				if (root_.Visible)
+				if (!inited_)
 				{
-					for (int i = 0; i < panels_.Count; ++i)
-						panels_[i].Update();
+					VUI.Glue.Set(
+						() => manager,
+						(s, ps) => string.Format(s, ps),
+						(s) => Log.Verbose(s),
+						(s) => Log.Info(s),
+						(s) => Log.Warning(s),
+						(s) => Log.Error(s));
 				}
 
-				root_?.Update();
-			}
-		}
-
-		private void DoUpdate()
-		{
-			var a = SuperController.singleton.GetAtomByUid("Person");
-			var mui = GetMUI(a);
-			morphs_ = mui.GetMorphs();
-
-			filtered_.Clear();
-			filtered_.Capacity = morphs_.Count;
-
-			// ghetto set
-			var set = new Dictionary<string, int>();
-
-			for (int i = 0; i < morphs_.Count; ++i)
-			{
-				var m = morphs_[i];
-
-				if (ShouldShow(m, set))
-				{
-					filtered_.Add(m);
-				}
-				else
-				{
-					SuperController.LogError($"filtered {m.uid}");
-				}
-
-			}
-
-			SuperController.LogError($"filtered {morphs_.Count - filtered_.Count} morphs");
-
-
-			for (int i = 0; i < panels_.Count; ++i)
-				panels_[i].SetMorph(filtered_[i]);
-		}
-
-		private bool ShouldShow(DAZMorph m, Dictionary<string, int> set)
-		{
-			if (m.morphValue != m.startValue)
-				return true;
-
-			if (!m.isLatestVersion)
-				return false;
-
-			var file = GetFilename(m);
-			if (set.ContainsKey(file))
-			{
-				SuperController.LogError($"dupe {m.uid} {file}");
-				return false;
-			}
-			else
-			{
-				set.Add(file, 0);
-			}
-
-			return true;
-		}
-
-		private string GetFilename(DAZMorph m)
-		{
-			if (m.isInPackage)
-			{
-				var pos = m.uid.IndexOf(":/");
-				if (pos == -1)
-				{
-					SuperController.LogError(
-						$"{m.uid} has no :/");
-				}
-				else
-				{
-					return m.uid.Substring(pos + 2);
-				}
-			}
-
-			return m.uid;
-		}
-
-		public GenerateDAZMorphsControlUI GetMUI(Atom atom)
-		{
-			if (atom == null)
-				return null;
-
-			var cs = atom.GetComponentInChildren<DAZCharacterSelector>();
-			if (cs == null)
-				return null;
-
-			return cs.morphsControlUI;
+				mui_.Update();
+			});
 		}
 
 		public void OnEnable()
@@ -286,6 +42,11 @@ namespace AUI
 
 		public void OnDisable()
 		{
+		}
+
+		public void DisablePlugin()
+		{
+			enabledJSON.val = false;
 		}
 
 		static void Main()
---
+++ project/src/Utilities.cs
@@ -0,0 +1,463 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+using UnityEngine;
+
+namespace AUI
+{
+	static class Bits
+	{
+		public static bool IsSet(int flag, int bits)
+		{
+			return ((flag & bits) == bits);
+		}
+
+		public static int Bit(int pos)
+		{
+			return (1 << pos);
+		}
+	}
+
+
+	static class Strings
+	{
+		public static string Get(string s, params object[] ps)
+		{
+			if (ps.Length > 0)
+				return string.Format(s, ps);
+			else
+				return s;
+		}
+	}
+
+
+	static class HashHelper
+	{
+		public static int GetHashCode<T1, T2>(T1 arg1, T2 arg2)
+		{
+			unchecked
+			{
+				return 31 * arg1.GetHashCode() + arg2.GetHashCode();
+			}
+		}
+
+		public static int GetHashCode<T1, T2, T3>(T1 arg1, T2 arg2, T3 arg3)
+		{
+			unchecked
+			{
+				int hash = arg1.GetHashCode();
+				hash = 31 * hash + arg2.GetHashCode();
+				return 31 * hash + arg3.GetHashCode();
+			}
+		}
+
+		public static int GetHashCode<T1, T2, T3, T4>(T1 arg1, T2 arg2, T3 arg3,
+			T4 arg4)
+		{
+			unchecked
+			{
+				int hash = arg1.GetHashCode();
+				hash = 31 * hash + arg2.GetHashCode();
+				hash = 31 * hash + arg3.GetHashCode();
+				return 31 * hash + arg4.GetHashCode();
+			}
+		}
+	}
+
+
+	class U
+	{
+		private static float lastErrorTime_ = 0;
+		private static int errorCount_ = 0;
+		private const int MaxErrors = 3;
+
+		public static void Safe(Action a)
+		{
+			try
+			{
+				a();
+			}
+			catch (Exception e)
+			{
+				Log.Error(e.ToString());
+
+				var now = Time.realtimeSinceStartup;
+
+				if (now - lastErrorTime_ < 1)
+				{
+					++errorCount_;
+					if (errorCount_ > MaxErrors)
+					{
+						Log.Error(
+							$"more than {MaxErrors} errors in the last " +
+							"second, disabling plugin");
+
+						AlternateUI.Instance.DisablePlugin();
+					}
+				}
+				else
+				{
+					errorCount_ = 0;
+				}
+
+				lastErrorTime_ = now;
+			}
+		}
+
+		public static T Clamp<T>(T val, T min, T max)
+			where T : IComparable<T>
+		{
+			if (val.CompareTo(min) < 0)
+				return min;
+			else if (val.CompareTo(max) > 0)
+				return max;
+			else
+				return val;
+		}
+
+		public static void NatSort(List<string> list)
+		{
+			list.Sort(new NaturalStringComparer());
+		}
+
+		public static void NatSort<T>(List<T> list)
+		{
+			list.Sort(new GenericNaturalStringComparer<T>());
+		}
+	}
+
+
+	// from https://stackoverflow.com/questions/248603
+	public class NaturalStringComparer : IComparer<string>
+	{
+		private static readonly Regex _re =
+			new Regex(@"(?<=\D)(?=\d)|(?<=\d)(?=\D)", RegexOptions.Compiled);
+
+		public int Compare(string x, string y)
+		{
+			x = x.ToLower();
+			y = y.ToLower();
+			if (string.Compare(x, 0, y, 0, Math.Min(x.Length, y.Length)) == 0)
+			{
+				if (x.Length == y.Length) return 0;
+				return x.Length < y.Length ? -1 : 1;
+			}
+			var a = _re.Split(x);
+			var b = _re.Split(y);
+			int i = 0;
+			while (true)
+			{
+				int r = PartCompare(a[i], b[i]);
+				if (r != 0)
+					return r;
+
+				if (a[i] != b[i])
+					return string.Compare(a[i], b[i]);
+
+				++i;
+			}
+		}
+
+		private static int PartCompare(string x, string y)
+		{
+			int a, b;
+			if (int.TryParse(x, out a) && int.TryParse(y, out b))
+				return a.CompareTo(b);
+			return x.CompareTo(y);
+		}
+	}
+
+
+	// from https://stackoverflow.com/questions/248603
+	public class GenericNaturalStringComparer<T> : IComparer<T>
+	{
+		private static readonly Regex _re =
+			new Regex(@"(?<=\D)(?=\d)|(?<=\d)(?=\D)", RegexOptions.Compiled);
+
+		public int Compare(T xt, T yt)
+		{
+			string x = xt.ToString();
+			string y = yt.ToString();
+
+			x = x.ToLower();
+			y = y.ToLower();
+			if (string.Compare(x, 0, y, 0, Math.Min(x.Length, y.Length)) == 0)
+			{
+				if (x.Length == y.Length) return 0;
+				return x.Length < y.Length ? -1 : 1;
+			}
+			var a = _re.Split(x);
+			var b = _re.Split(y);
+			int i = 0;
+			while (true)
+			{
+				int r = PartCompare(a[i], b[i]);
+				if (r != 0)
+					return r;
+
+				if (a[i] != b[i])
+					return string.Compare(a[i], b[i]);
+
+				++i;
+			}
+		}
+
+		private static int PartCompare(string x, string y)
+		{
+			int a, b;
+			if (int.TryParse(x, out a) && int.TryParse(y, out b))
+				return a.CompareTo(b);
+			return x.CompareTo(y);
+		}
+	}
+
+
+	class Ticker
+	{
+		private readonly string name_;
+		private Stopwatch w_ = new Stopwatch();
+		private long freq_ = Stopwatch.Frequency;
+		private long ticks_ = 0;
+		private long calls_ = 0;
+
+		private float elapsed_ = 0;
+		private long avg_ = 0;
+		private long peak_ = 0;
+
+		private long lastPeak_ = 0;
+		private long lastCalls_ = 0;
+		private bool updated_ = false;
+
+		public Ticker(string name = "")
+		{
+			name_ = name;
+		}
+
+		public string Name
+		{
+			get { return name_; }
+		}
+
+		public void Do(Action f)
+		{
+			updated_ = false;
+
+			w_.Reset();
+			w_.Start();
+			f();
+			w_.Stop();
+
+			++calls_;
+			ticks_ += w_.ElapsedTicks;
+			peak_ = Math.Max(peak_, w_.ElapsedTicks);
+		}
+
+		public void Update(float s)
+		{
+			elapsed_ += s;
+			if (elapsed_ >= 1)
+			{
+				if (calls_ <= 0)
+					avg_ = 0;
+				else
+					avg_ = ticks_ / calls_;
+
+				lastPeak_ = peak_;
+				lastCalls_ = calls_;
+
+				ticks_ = 0;
+				calls_ = 0;
+				elapsed_ = 0;
+				peak_ = 0;
+				updated_ = true;
+			}
+		}
+
+		public bool Updated
+		{
+			get { return updated_; }
+		}
+
+		public float AverageMs
+		{
+			get
+			{
+				return ToMs(avg_);
+			}
+		}
+
+		public float PeakMS
+		{
+			get { return ToMs(lastPeak_); }
+		}
+
+		private float ToMs(long ticks)
+		{
+			return (float)((((double)ticks) / freq_) * 1000);
+		}
+
+		public long Calls
+		{
+			get { return lastCalls_; }
+		}
+
+		public override string ToString()
+		{
+			return $"calls={Calls} avg={AverageMs:0.000} peak={PeakMS:0.000}";
+		}
+	}
+
+
+	public class IgnoreFlag
+	{
+		private bool ignore_ = false;
+
+		public static implicit operator bool(IgnoreFlag f)
+		{
+			return f.ignore_;
+		}
+
+		public void Do(Action a)
+		{
+			try
+			{
+				ignore_ = true;
+				a();
+			}
+			finally
+			{
+				ignore_ = false;
+			}
+		}
+	}
+
+
+	static class Log
+	{
+		public const int ErrorLevel = 0;
+		public const int WarningLevel = 1;
+		public const int InfoLevel = 2;
+		public const int VerboseLevel = 3;
+
+		public static string LevelToShortString(int i)
+		{
+			switch (i)
+			{
+				case ErrorLevel: return "E";
+				case WarningLevel: return "W";
+				case InfoLevel: return "I";
+				case VerboseLevel: return "V";
+				default: return $"?{i}";
+			}
+		}
+
+		public static void Out(int level, string s)
+		{
+			var t = DateTime.Now.ToString("hh:mm:ss.fff");
+			string p = LevelToShortString(level);
+
+			if (level == ErrorLevel)
+				SuperController.LogError($"{t} !![{p}] {s}");
+			else
+				SuperController.LogError($"{t}   [{p}] {s}");
+		}
+
+		public static void Verbose(string s)
+		{
+			Out(VerboseLevel, s);
+		}
+
+		public static void Info(string s)
+		{
+			Out(InfoLevel, s);
+		}
+
+		public static void Warning(string s)
+		{
+			Out(WarningLevel, s);
+		}
+
+		public static void Error(string s)
+		{
+			Out(ErrorLevel, s);
+		}
+
+		public static void ErrorST(string s)
+		{
+			Out(ErrorLevel, $"{s}\n{new StackTrace(1)}");
+		}
+	}
+
+
+	class Logger
+	{
+		public const int All = int.MaxValue;
+		private static int enabled_ = All;
+
+		private int type_;
+		private Func<string> prefix_;
+
+		public Logger(int type, string prefix)
+		{
+			type_ = type;
+			prefix_ = () => prefix;
+		}
+
+		public Logger(int type, Func<string> prefix)
+		{
+			type_ = type;
+			prefix_ = prefix;
+		}
+
+		public Logger(int type, Atom a, string prefix)
+		{
+			type_ = type;
+			prefix_ = () => a.uid + (prefix == "" ? "" : " " + prefix);
+		}
+
+		public static int Enabled
+		{
+			get { return enabled_; }
+			set { enabled_ = value; }
+		}
+
+		public string Prefix
+		{
+			get { return prefix_(); }
+		}
+
+		public void Verbose(string s)
+		{
+			if (IsEnabled())
+				Log.Verbose($"{Prefix}: {s}");
+		}
+
+		public void Info(string s)
+		{
+			if (IsEnabled())
+				Log.Info($"{Prefix}: {s}");
+		}
+
+		public void Warning(string s)
+		{
+			Log.Warning($"{Prefix}: {s}");
+		}
+
+		public void Error(string s)
+		{
+			Log.Error($"{Prefix}: {s}");
+		}
+
+		public void ErrorST(string s)
+		{
+			Log.ErrorST($"{Prefix}: {s}\n{new StackTrace(1)}");
+		}
+
+		private bool IsEnabled()
+		{
+			return Bits.IsSet(enabled_, type_);
+		}
+	}
+}
