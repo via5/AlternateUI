feee0350ca936ab6e5c0692c1558e1296b0a84e8
via5 <65577279+via5@users.noreply.github.com>
2023-01-26 06:44:34 -0500

    1.0

--- project/src/Version.cs
+++ project/src/Version.cs
@@ -2,7 +2,7 @@
 {
 	static class Version
 	{
-		public static int Major { get { return 2; } }
+		public static int Major { get { return 1; } }
 		public static int Minor { get { return 0; } }
 		public static string String { get { return $"{Major}.{Minor}"; } }
 	}
