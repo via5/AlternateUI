5b3f2b584a1017397b035269e9572747c09e343a
via5 <65577279+via5@users.noreply.github.com>
2023-01-29 09:33:49 -0500

    reorganizing

--- project/src/Utilities.cs
+++ project/src/Utilities.cs
@@ -1,10 +1,8 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Text.RegularExpressions;
 using System.Globalization;
-using UnityEngine;
 
 namespace AUI
 {
