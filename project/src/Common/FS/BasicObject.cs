d380dab0f515b1db96434c7ef0fc38d386390c09
via5 <65577279+via5@users.noreply.github.com>
2023-03-11 14:27:37 -0500

    split

---
+++ project/src/Common/FS/BasicObject.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Globalization;
+
+namespace AUI.FS
+{
+	abstract class BasicFilesystemObject : IFilesystemObject
+	{
+		protected readonly Filesystem fs_;
+		private readonly IFilesystemContainer parent_;
+		private string displayName_ = null;
+
+		public BasicFilesystemObject(
+			Filesystem fs, IFilesystemContainer parent,
+			string displayName = null)
+		{
+			fs_ = fs;
+			parent_ = parent;
+			displayName_ = displayName;
+		}
+
+		public IFilesystemContainer Parent
+		{
+			get { return parent_; }
+		}
+
+		public string DisplayName
+		{
+			get
+			{
+				return displayName_ ?? GetDisplayName();
+			}
+
+			set
+			{
+				if (displayName_ != value)
+				{
+					displayName_ = value;
+					DisplayNameChanged();
+				}
+			}
+		}
+
+		public bool HasCustomDisplayName
+		{
+			get { return (displayName_ != null); }
+		}
+
+		public virtual string VirtualPath
+		{
+			get
+			{
+				string s = Name;
+
+				var parent = parent_;
+				while (parent != null)
+				{
+					s = parent.Name + "/" + s;
+					parent = parent.Parent;
+				}
+
+				return s;
+			}
+		}
+
+		public virtual IPackage ParentPackage
+		{
+			get
+			{
+				IFilesystemObject o = this;
+
+				while (o != null)
+				{
+					if (o is IPackage)
+						return o as IPackage;
+
+					o = o.Parent;
+				}
+
+				return null;
+			}
+		}
+
+		public abstract string Name { get; }
+		public abstract DateTime DateCreated { get; }
+		public abstract DateTime DateModified { get; }
+		public abstract VUI.Icon Icon { get; }
+		public abstract bool CanPin { get; }
+		public abstract bool Virtual { get; }
+		public abstract bool ChildrenVirtual { get; }
+		public abstract bool IsFlattened { get; }
+
+		public virtual bool IsRedundant
+		{
+			get { return false; }
+		}
+
+
+		public virtual string Tooltip
+		{
+			get
+			{
+				string tt =
+					$"{base.ToString()}\n" +
+					$"Virtual path: {VirtualPath}" +
+					$"\nReal path: {(Virtual ? "(virtual)" : MakeRealPath())}";
+
+				var p = ParentPackage;
+
+				if (p != null)
+					tt += $"\nPackage: {p.DisplayName}";
+
+				tt += $"\nCreated: {FormatDT(DateCreated)}";
+				tt += $"\nLast modified: {FormatDT(DateModified)}";
+
+				return tt;
+			}
+		}
+
+		private string FormatDT(DateTime dt)
+		{
+			return dt.ToString(CultureInfo.CurrentCulture);
+		}
+
+		public virtual bool IsSameObject(IFilesystemObject o)
+		{
+			return fs_.IsSameObject(this, o);
+		}
+
+		public virtual IFilesystemObject Resolve(
+			Context cx, string path, int flags = Filesystem.ResolveDefault)
+		{
+			if (path == Name)
+				return this;
+
+			return null;
+		}
+
+		public abstract string MakeRealPath();
+
+		public virtual void ClearCache()
+		{
+			// no-op
+		}
+
+		protected virtual string GetDisplayName()
+		{
+			return Name;
+		}
+
+		protected virtual void DisplayNameChanged()
+		{
+			// no-op
+		}
+	}
+}
---
+++ project/src/Common/FS/Cache.cs
@@ -0,0 +1,166 @@
+﻿using System.Collections.Generic;
+
+namespace AUI.FS
+{
+	class CachedListing<T> where T : class, IFilesystemObject
+	{
+		private int cacheToken_ = -1;
+		private bool showHiddenFolders_ = false;
+		private bool showHiddenFiles_ = false;
+		private Listing<T> listing_ = null;
+
+		public bool ShowHiddenFolders
+		{
+			get { return showHiddenFolders_; }
+		}
+
+		public bool ShowHiddenFiles
+		{
+			get { return showHiddenFiles_; }
+		}
+
+		public Listing<T> Listing
+		{
+			get { return listing_; }
+		}
+
+		public void Clear()
+		{
+			cacheToken_ = -1;
+		}
+
+		public bool Stale(Filesystem fs)
+		{
+			if (cacheToken_ != fs.CacheToken)
+				return true;
+
+			if (listing_?.Raw == null)
+				return true;
+
+			return false;
+		}
+
+		public void SetRaw(Filesystem fs, Context cx, List<T> list)
+		{
+			cacheToken_ = fs.CacheToken;
+			showHiddenFolders_ = cx.ShowHiddenFolders;
+			showHiddenFiles_ = cx.ShowHiddenFiles;
+
+			if (listing_ == null)
+				listing_ = new Listing<T>();
+
+			listing_.SetRaw(list);
+		}
+	}
+
+
+	class Cache
+	{
+		private CachedListing<IFilesystemContainer> localDirs_;
+		private CachedListing<IFilesystemObject> localFiles_;
+		private CachedListing<IFilesystemObject> recursiveFiles_;
+
+		public void Clear()
+		{
+			localDirs_?.Clear();
+			localFiles_?.Clear();
+			recursiveFiles_?.Clear();
+		}
+
+		public bool StaleLocalDirectoriesCache(Filesystem fs, Context cx)
+		{
+			if (localDirs_?.Stale(fs) ?? true)
+				return true;
+
+			if (localDirs_.ShowHiddenFolders != cx.ShowHiddenFolders)
+				return true;
+
+			return false;
+		}
+
+		public void SetLocalDirectoriesCache(
+			Filesystem fs, Context cx, List<IFilesystemContainer> list)
+		{
+			if (localDirs_ == null)
+				localDirs_ = new CachedListing<IFilesystemContainer>();
+
+			localDirs_.SetRaw(fs, cx, list);
+		}
+
+		public Listing<IFilesystemContainer> GetLocalDirectories()
+		{
+			return localDirs_.Listing;
+		}
+
+		public Listing<IFilesystemContainer> GetLocalDirectoriesCache()
+		{
+			if (localDirs_ == null)
+				localDirs_ = new CachedListing<IFilesystemContainer>();
+
+			return localDirs_.Listing;
+		}
+
+
+		public bool StaleLocalFilesCache(Filesystem fs, Context cx)
+		{
+			if (localFiles_?.Stale(fs) ?? true)
+				return true;
+
+			if (localFiles_.ShowHiddenFolders != cx.ShowHiddenFolders)
+				return true;
+
+			if (localFiles_.ShowHiddenFiles != cx.ShowHiddenFiles)
+				return true;
+
+			return false;
+		}
+
+		public void SetLocalFilesCache(
+			Filesystem fs, Context cx, List<IFilesystemObject> list)
+		{
+			if (localFiles_ == null)
+				localFiles_ = new CachedListing<IFilesystemObject>();
+
+			localFiles_.SetRaw(fs, cx, list);
+		}
+
+		public Listing<IFilesystemObject> GetLocalFilesCache()
+		{
+			if (localFiles_ == null)
+				localFiles_ = new CachedListing<IFilesystemObject>();
+
+			return localFiles_.Listing;
+		}
+
+
+		public bool StaleRecursiveFilesCache(Filesystem fs, Context cx)
+		{
+			if (recursiveFiles_?.Stale(fs) ?? true)
+				return true;
+
+			if (recursiveFiles_.ShowHiddenFolders != cx.ShowHiddenFolders)
+				return true;
+
+			if (recursiveFiles_.ShowHiddenFiles != cx.ShowHiddenFiles)
+				return true;
+
+			return false;
+		}
+
+		public void SetRecursiveFilesCache(Filesystem fs, Context cx)
+		{
+			if (recursiveFiles_ == null)
+				recursiveFiles_ = new CachedListing<IFilesystemObject>();
+
+			recursiveFiles_.SetRaw(fs, cx, null);
+		}
+
+		public Listing<IFilesystemObject> GetRecursiveFilesCache()
+		{
+			if (recursiveFiles_ == null)
+				recursiveFiles_ = new CachedListing<IFilesystemObject>();
+
+			return recursiveFiles_.Listing;
+		}
+	}
+}
